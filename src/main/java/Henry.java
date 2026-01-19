e514a2740cdead5171a67b1df21d5ef424201859
wjchew <95748108+happyweijie@users.noreply.github.com>
2026-01-19 19:48:15 +0800

    Rename Duke to Henry

---
+++ src/main/java/Henry.java
@@ -0,0 +1,8 @@
+public class Henry {
+    private static final String LOGO = "--henry";
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Henry" + LOGO);
+    }
+}
