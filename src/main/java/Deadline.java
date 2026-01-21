5017c602a084ad7d0c29d6d655f0e9b3bf9626c7
wjchew <95748108+happyweijie@users.noreply.github.com>
2026-01-21 14:12:28 +0800

    Rename date attribute to by in Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,14 @@
 public class Deadline extends Task {
-    protected String date;
+    protected String by;
 
-    public Deadline(String description, String date) {
+    public Deadline(String description, String by) {
         super(description);
-        this.date = date;
+        this.by = by;
     }
 
     @Override
     public String toString() {
         return String.format("[D]%s (by: %s)",
-                super.toString(), this.date);
+                super.toString(), this.by);
     }
 }
