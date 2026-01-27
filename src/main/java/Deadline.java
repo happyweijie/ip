5d1b1f9fa2c1def539bea1ba917d0a5d0efdc9d1
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-01-27 21:20:25 +0800

    Update by field in deadline to be Date Object

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,33 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    private static final DateTimeFormatter OUTPUT_FORMAT= DateTimeFormatter.ofPattern(
+            "MMM dd yyyy");
+
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
+
     }
 
     public Deadline(String description, boolean isDone,
                     String by) {
         super(description, isDone);
-        this.by = by;
+        this.by = LocalDate.parse(by);
+    }
+
+    private static LocalDate parseDate(String date) {
+        try {
+            // Parse using yyyy-mm-dd
+            return LocalDate.parse(date);
+        } catch (DateTimeParseException d) {
+            // Set to today's date by default
+            return LocalDate.now();
+        }
     }
 
     @Override
@@ -17,13 +35,13 @@ public class Deadline extends Task {
         return "D";
     }
 
-    public String getBy() {
+    public LocalDate getBy() {
         return by;
     }
 
     @Override
     public String toString() {
         return String.format("[D]%s (by: %s)",
-                super.toString(), this.by);
+                super.toString(), this.by.format(OUTPUT_FORMAT));
     }
 }
