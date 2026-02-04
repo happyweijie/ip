eca2ca5ac4444b21d150335aa3fbc0d9bd3c67e2
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-02-04 19:55:49 +0800

    Organise import statement

--- src/main/java/jimjam/ui/MainWindow.java
+++ src/main/java/jimjam/ui/MainWindow.java
@@ -9,8 +9,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-
 import javafx.util.Duration;
+
 import jimjam.exception.ExitException;
 import jimjam.exception.JimjamException;
 import jimjam.jimjam.Jimjam;
