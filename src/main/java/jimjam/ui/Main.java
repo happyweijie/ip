d464be151bc4ab7da6a194e4f65bd5e41fad60ea
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-02-07 15:14:27 +0800

    Add window title to GUI

--- src/main/java/jimjam/ui/Main.java
+++ src/main/java/jimjam/ui/Main.java
@@ -30,6 +30,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Jimjam");
             fxmlLoader.<MainWindow>getController().setJimjam(jimjam);  // inject the Jimjam instance
             stage.show();
         } catch (IOException e) {
