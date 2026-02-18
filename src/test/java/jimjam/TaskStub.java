459cca491865ef21fdc4a79d74147c3b95ad95a6
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-02-18 10:11:13 +0800

    Fix indentation issues for tests

--- src/test/java/jimjam/TaskStub.java
+++ src/test/java/jimjam/TaskStub.java
@@ -7,20 +7,20 @@ import java.util.Optional;
 
 public class TaskStub extends Task {
     public TaskStub(String description) {
-	    super(description);
+        super(description);
     }
 
-	public TaskStub(String description, boolean isDone) {
-		super(description, isDone);
-	}
+    public TaskStub(String description, boolean isDone) {
+        super(description, isDone);
+    }
 
-	@Override
-	public String getIcon() {
-		return "S"; // S for Stub
-	}
+    @Override
+    public String getIcon() {
+        return "S"; // S for Stub
+    }
 
-	@Override
-	public Optional<LocalDate> getRelevantDate() {
-		return Optional.empty();
-	}
+    @Override
+    public Optional<LocalDate> getRelevantDate() {
+        return Optional.empty();
+    }
 }
--- src/test/java/jimjam/logic/ParserTest.java
+++ src/test/java/jimjam/logic/ParserTest.java
@@ -10,43 +10,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParserTest {
-	@Test
-	public void parseFileLine_validDate_success() throws JimjamException {
-		assertEquals(
-				LocalDate.of(2026, 1, 28),
-				Parser.parseDate("2026-01-28")
-		);
-	}
+    @Test
+    public void parseFileLine_validDate_success() throws JimjamException {
+        assertEquals(
+                LocalDate.of(2026, 1, 28),
+                Parser.parseDate("2026-01-28")
+        );
+    }
 
-	@Test
-	public void parseFileLine_invalidDateFormat_exceptionThrown() {
-		assertThrows(
-				JimjamException.class,
-				() -> Parser.parseDate("28-01-2026")
-		);
-	}
+    @Test
+    public void parseFileLine_invalidDateFormat_exceptionThrown() {
+        assertThrows(
+                JimjamException.class,
+                () -> Parser.parseDate("28-01-2026")
+        );
+    }
 
-	@Test
-	public void parseFileLine_invalidString_exceptionThrown() {
-		assertThrows(
-				JimjamException.class,
-				() -> Parser.parseDate("not-a-date")
-		);
-	}
+    @Test
+    public void parseFileLine_invalidString_exceptionThrown() {
+        assertThrows(
+                JimjamException.class,
+                () -> Parser.parseDate("not-a-date")
+        );
+    }
 
-	@Test
-	public void taskToFileLine_unmarkedTask_success() {
-		assertEquals(
-				"S | 0 | test",
-				Parser.taskToFileLine(new TaskStub("test"))
-		);
-	}
+    @Test
+    public void taskToFileLine_unmarkedTask_success() {
+        assertEquals(
+                "S | 0 | test",
+                Parser.taskToFileLine(new TaskStub("test"))
+        );
+    }
 
-	@Test
-	public void taskToFileLine_markedTask_success() {
-		assertEquals(
-				"S | 1 | test",
-				Parser.taskToFileLine(new TaskStub("test", true))
-		);
-	}
+    @Test
+    public void taskToFileLine_markedTask_success() {
+        assertEquals(
+                "S | 1 | test",
+                Parser.taskToFileLine(new TaskStub("test", true))
+        );
+    }
 }
